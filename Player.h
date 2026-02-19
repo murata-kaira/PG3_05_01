e3995ae7341efb09648a5b42f7dbeff5028e7408
murata-kaira <k024g0062@g.neec.ac.jp>
2026-02-19 21:13:32 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Vector2.h"
+
+
+
+//プレイヤー
+class Player {
+
+private:
+
+	// 位置
+	Vector2 position_;
+
+	// 速さ
+	float speed_ = 1.5f;
+
+public:
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+	/// 右移動
+	void MoveRight();
+
+	/// 左移動
+	void MoveLeft();
+};
